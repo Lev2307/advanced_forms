740fd8d0767c5e52d256f2b0f014832b513fcc27
Lev2307 <levchik23072007@gmail.com>
2022-02-25 18:23:25 +0300

    upd

--- pizza/config/urls.py
+++ pizza/config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from features.views import index
+from order.views import create_order
 import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('create_order/', create_order, name="createorder"),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
